b653f08ca4a1948514da62c2a633537741383745
Gerard Du Pre <37554513+gerardDuPre@users.noreply.github.com>
2023-01-23 12:36:03 +0100

    Add files via upload

---
+++ Excercici3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Excercici3 {
+    public static void main(String[] args) {
+
+        Scanner llegir = new Scanner(System.in);
+
+        float costInicial = 0.0f , costFinal = 0.0f, preuVenda = 0.0f;
+        boolean bonEstat;
+
+
+        System.out.println("Benvingut/da al programa...");
+        System.out.print("Escriu l'import de la compra actual: ");
+        costInicial = llegir.nextFloat();
+
+        System.out.print("La figura esta en bon estat? (true | false) ");
+        bonEstat = llegir.nextBoolean();
+
+        if(bonEstat == true) {
+
+            preuVenda = costInicial * 1.25f;
+
+        }else {
+            preuVenda = costInicial * 1.10f;
+        }
+        System.out.println("El preu de venda d ela figura es " + preuVenda);
+
+    }
+}
